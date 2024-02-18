1d79e2a71537fcf2101ebf45d57c355d802e8098
bonxatiwat <bon.atiwat@gmail.com>
2024-02-18 22:00:50 +0700

    feat: Router Check

--- modules/middlewares/middlewareHandlers/middlewareHandlers.go
+++ modules/middlewares/middlewareHandlers/middlewareHandlers.go
@@ -2,13 +2,21 @@ package middlewareHandlers
 
 import (
 	"github.com/bonxatiwat/kawaii-shop-tutortial/config"
+	"github.com/bonxatiwat/kawaii-shop-tutortial/modules/entities"
 	"github.com/bonxatiwat/kawaii-shop-tutortial/modules/middlewares/middlewareUsecases"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
+type middlewareHandlersErrCode string
+
+const (
+	routerCheckErr middlewareHandlersErrCode = "middleware-001"
+)
+
 type IMiddelwaresHandler interface {
 	Cors() fiber.Handler
+	RouterCheck() fiber.Handler
 }
 
 type middlewaresHandler struct {
@@ -33,4 +41,14 @@ func (h *middlewaresHandler) Cors() fiber.Handler {
 		ExposeHeaders:    "",
 		MaxAge:           0,
 	})
+}
+
+func (h *middlewaresHandler) RouterCheck() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		return entities.NewResponse(c).Error(
+			fiber.ErrNotFound.Code,
+			string(routerCheckErr),
+			"routter not found",
+		).Res()
+	}
 }
--- modules/servers/servers.go
+++ modules/servers/servers.go
@@ -49,6 +49,8 @@ func (s *server) Start() {
 
 	modules.MonitorModule()
 
+	s.app.Use(middlewares.RouterCheck())
+
 	// Graceful Shutdown
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
